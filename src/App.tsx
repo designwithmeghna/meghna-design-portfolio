9f24e76749bb135589ad0f96e27b68fac2f67e2b
designwithmeghna <designwithmeghna@gmail.com>
2024-08-03 22:02:10 -0400

    mini games remove

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import NoPage from "./pages/NoPage";
 
 import "./App.css";
 import DigitalResume from "./pages/DigitalResume";
-import MiniGames from "./pages/MiniGames";
-import CardMatching from "./pages/CardMatching";
-import StonePaperScissors from "./pages/StonePaperScissors";
-import WordGuess from "./pages/WordGuess";
 // import ComingSoon from "./pages/ComingSoon";
 
 function App() {
@@ -18,17 +14,6 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/mini-games" element={<MiniGames />} />
-            <Route
-              path="/mini-games/card-matching"
-              element={<CardMatching />}
-            />
-            <Route
-              path="/mini-games/stone-paper-scissors"
-              element={<StonePaperScissors />}
-            />
-            <Route path="/mini-games/word-guess" element={<WordGuess />} />
-
             <Route path="/digital-resume" element={<DigitalResume />} />
             <Route index path="/" element={<Home />} />
             <Route path="*" element={<NoPage />} />
